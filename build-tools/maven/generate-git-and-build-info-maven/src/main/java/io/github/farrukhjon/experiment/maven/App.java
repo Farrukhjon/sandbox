55f2eeffe378150bf26ea91988178dafff44f5c4
Farrukhjon Sattorov <farrukhjon.sattorov@alertinnovation.com>
2022-10-31 12:56:58 +0500

    Add git-commit-id-maven-plugin experiment

---
+++ build-tools/maven/generate-git-and-build-info-maven/src/main/java/io/github/farrukhjon/experiment/maven/App.java
@@ -0,0 +1,12 @@
+package io.github.farrukhjon.experiment.maven;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+  @SuppressWarnings("java:S106")
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}

0a145cf58019443be0a3f788338329bbdd4d4e69
Farrukhjon Sattorov <farrukhjon_sattorov@epam.com>
2025-03-17 15:37:41 +0500

    Cleanup

--- testing/mongodb-memory-server-js/test/dot-env.test.js
+++
@@ -1,7 +0,0 @@
-describe('Dotenv test suit', () => {
-    test('Dotenv test', () => {
-        expect(process.env.MONGOMS_VERSION).toEqual('8.0.0')
-        expect(process.env.MONGOMS_DOWNLOAD_URL).toEqual('https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-8.0.0.tgz')
-    });
-});
-
